2a507e94c997ef2f8610cdc0d7156452374a7aa7
DESKTOP-E9817SU\admin <apimpale06@gmail.com>
2024-03-06 17:54:14 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'sign_Languages',
+    version= '0.0.0',
+    author= 'Abhijeet',
+    author_email= 'apimpale06@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
